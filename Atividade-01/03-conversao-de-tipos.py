987ea2e1a1f5be4eb66d5a7a320803e06871f80e
Rafael Cerqueira <rafaelcerqueira92@gmail.com>
2023-08-15 19:17:33 -0300

    Conversao de tipo de dados

--- Atividade-01/03-conversao-de-tipos.py
+++ Atividade-01/03-conversao-de-tipos.py
@@ -1,4 +1,6 @@
 """
+Conversão de tipos de dados:
+
     Inteiro: int()
     Texto: str()
     Decimal: float()
@@ -8,3 +10,20 @@
     Dicionário: dic()
     
 """
+#Convertendo inteiro para decimal - int() e float():
+nota = 4
+print(nota) #Imprime: 4
+
+nota_decimal = float(nota)
+print(nota_decimal) #Imprime: 4.0
+
+nota1, nota2, nota3 = 6, 8, 9
+media = float((nota1 + nota2 + nota3) / 3)
+print(media) #Imprime: 7.666667
+
+#Convertendo decimal para inteiro - float() e int():
+peso = 53.70
+print(peso) #Imprime: 53.7
+
+peso_inteiro = int(peso)
+print(peso_inteiro) #Imprime: 53
