92435036282ca897b557ac51ee0f554795c58ef6
Rafael Cerqueira <rafaelcerqueira92@gmail.com>
2023-08-15 21:23:29 -0300

    Entendedo funções

---
+++ Atividade-01/04-funcoes.py
@@ -0,0 +1,11 @@
+"""
+- Função builtin input(): é uma função já existente no Python que é utilizada para ler dados de entrada feita pelo usuário.
+- Esta função lê a entrada, converte o valor para string (texto) e retorna o valor.
+
+"""
+
+# Exemplo de input():
+nome_do_usuario = input("Digite seu nome: ") #A função input() recebe os dados digitados pelo usuário no prompt. Note que a função é armazenada na variável "nome_do_usuario".
+
+print(nome_do_usuario) #Imprime na tela o valor que será digitado pelo usuário no prompt e convertido em string pela função input().
+
